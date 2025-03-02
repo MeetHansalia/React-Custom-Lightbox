328d96b1a21d0fd64feca2c456ff18736935ae2a
Meet <killerhyper2@gmail.com>
2025-03-02 19:05:45 +0530

    changes for custom close and side arrow

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ const ReactImageVideoLightbox = ({
     imageWidth,
     imageHeight,
     videoWidth,
-    videoHeight
+    videoHeight,
+    customCloseIcon, // Optional custom close icon
+    customLeftArrowIcon,// Optional custom left arrow icon
+    customRightArrowIcon,
 }) => {
     const [state, setState] = useState({
         x: INITIAL_X,
@@ -185,10 +188,12 @@ const ReactImageVideoLightbox = ({
                 }}
                 // onClick={onCloseCallback}
                 onClick={handleClose}>
-                <svg xmlns='http://www.w3.org/2000/svg' height='36px' viewBox='0 0 24 24' width='36px' fill='#FFFFFF'>
-                    <path d='M0 0h24v24H0z' fill='none' />
-                    <path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z' />
-                </svg>
+                {customCloseIcon ? customCloseIcon : (
+                    <svg xmlns='http://www.w3.org/2000/svg' height='36px' viewBox='0 0 24 24' width='36px' fill='#FFFFFF'>
+                        <path d='M0 0h24v24H0z' fill='none' />
+                        <path d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z' />
+                    </svg>
+                )}
             </div>
 
             {/* Navigation Arrows */}
@@ -203,10 +208,12 @@ const ReactImageVideoLightbox = ({
                         fontSize: `${state.iconSize}px`
                     }}
                     onClick={swipeLeft}>
-                    <svg xmlns='http://www.w3.org/2000/svg' height='48px' viewBox='0 0 24 24' width='48px' fill='#FFFFFF'>
-                        <path d='M0 0h24v24H0z' fill='none' />
-                        <path d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z' />
-                    </svg>
+                    {customLeftArrowIcon ? customLeftArrowIcon : (
+                        <svg xmlns='http://www.w3.org/2000/svg' height='48px' viewBox='0 0 24 24' width='48px' fill='#FFFFFF'>
+                            <path d='M0 0h24v24H0z' fill='none' />
+                            <path d='M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z' />
+                        </svg>
+                    )}
                 </div>
             )}
 
@@ -221,10 +228,12 @@ const ReactImageVideoLightbox = ({
                         fontSize: `${state.iconSize}px`
                     }}
                     onClick={swipeRight}>
-                    <svg xmlns='http://www.w3.org/2000/svg' height='48px' viewBox='0 0 24 24' width='48px' fill='#FFFFFF'>
-                        <path d='M0 0h24v24H0z' fill='none' />
-                        <path d='M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z' />
-                    </svg>
+                    {customRightArrowIcon ? customRightArrowIcon : (
+                        <svg xmlns='http://www.w3.org/2000/svg' height='48px' viewBox='0 0 24 24' width='48px' fill='#FFFFFF'>
+                            <path d='M0 0h24v24H0z' fill='none' />
+                            <path d='M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z' />
+                        </svg>
+                    )}
                 </div>
             )}
 
